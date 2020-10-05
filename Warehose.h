55be481b68830a596e1d72a795a2cdb42b6451b4
Master <50782066+Rikitaru@users.noreply.github.com>
2020-10-05 17:40:39 +0300

    Add files via upload

---
+++ Warehose.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Chain.h"
+
+class Warehouse : public Сhain {
+protected:
+	string filename;
+	void result(Game* game);
+
+public:
+
+	Warehouse(); //конструктор
+	string get_filename();
+};
